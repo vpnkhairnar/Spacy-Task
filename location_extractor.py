e6c4fe9bf94435128954d8665b92e7afb9643ef0
vpnkhairnar <124508766+vpnkhairnar@users.noreply.github.com>
2025-06-12 12:59:40 +0530

    Update location_extractor.py

--- location_extractor.py
+++ location_extractor.py
@@ -1,1 +1,11 @@
+import spacy
+
+# Load SpaCy English model
+nlp = spacy.load("en_core_web_trf")
+
+def extract_locations(text):
+    doc = nlp(text)
+    locations = [ent.text for ent in doc.ents if ent.label_ in ("GPE", "LOC")]
+    return locations
+
 
