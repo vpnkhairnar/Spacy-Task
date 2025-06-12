85e412dcf8bf1c7e0437468fb9602d9fd40c3620
vpnkhairnar <124508766+vpnkhairnar@users.noreply.github.com>
2025-06-12 12:59:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+from location_extractor import extract_locations
+
+def run_cli():
+    print("Location Extractor")
+    while True:
+        user_input = input("\nEnter a sentence (or type 'exit' to quit):\n> ")
+        if user_input.lower() == "exit":
+            break
+        locations = extract_locations(user_input)
+        if locations:
+           print(f" Extracted Locations: {locations}")
+        else:
+            print("No locations found.")
+
+if __name__ == "__main__":
+    run_cli()
+
 
